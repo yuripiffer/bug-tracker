1e25f62081475315a475d9f8ec0f60cd0eff6786
james-willett <james.willett58@gmail.com>
2025-01-30 09:25:22 +0000

    Getting perf test results report working locally

--- tests-perf/script.js
+++ tests-perf/script.js
@@ -1,5 +1,7 @@
 import http from "k6/http";
 import { sleep, check } from "k6";
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 
 export const options = {
   duration: "30s",
@@ -37,4 +39,11 @@ export default function () {
   });
 
   sleep(5);
+}
+
+export function handleSummary(data) {
+  return {
+    "test-results/perf-results.html": htmlReport(data),
+    stdout: textSummary(data, { indent: " ", enableColors: true }),
+  };
 }
